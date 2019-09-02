3b26f7b6bfb8547df9516baaf36b81bc37b5a107
Md Sazid Khan <mdsazidkhan18@gmail.com>
2019-09-03 01:53:31 +0530

    mac changer

---
+++ mac_changer/mac_changer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import subprocess
+
+interface = input("inteface > ")
+new_mac = input("new MAC > ")
+
+print("[+] Changing MAC address for"+interface+"to"+new_mac)
+
+subprocess.call(["ifconfig",interface,"down"])
+subprocess.call(["ifconfig",interface,"hw","ether",new_mac])
+subprocess.call(["ifconfig",interface,"up"])
